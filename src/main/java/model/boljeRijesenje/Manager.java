6bdeb097cb48577aeb042d78ccf3247509a43594
danilojoncic <djoncic14122rn@raf.rs>
2023-11-16 00:25:37 +0100

    Manager dodat

--- src/main/java/model/boljeRijesenje/Manager.java
+++ src/main/java/model/boljeRijesenje/Manager.java
@@ -1,4 +1,15 @@
 package model.boljeRijesenje;
 
+import model.Specifikacija;
+
 public class Manager {
+    private static Specifikacija specifikacija;
+
+    public static Specifikacija getSpecifikacija() {
+        return specifikacija;
+    }
+
+    public static void setSpecifikacija(Specifikacija specifikacija) {
+        specifikacija = specifikacija;
+    }
 }
