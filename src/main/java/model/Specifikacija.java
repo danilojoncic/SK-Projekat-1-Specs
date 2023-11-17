59b2b693280da752688498d34c1d9437ec2a75f1
danilojoncic <djoncic14122rn@raf.rs>
2023-11-17 12:52:48 +0100

    pom xml i neka dokumentacija

--- src/main/java/model/Specifikacija.java
+++ src/main/java/model/Specifikacija.java
@@ -2,12 +2,45 @@ package model;
 
 import model.boljeRijesenje.Raspored;
 
-public interface Specifikacija {
+import java.util.Scanner;
 
+/**
+ * klasa Specifiakcija predstavlja interfejs preko kojeg se mogu izvrsavati metode iz implementacija
+ * primarna svhra ovog interfejsa jeste rad nad genericnim rasporedima i hardkodovanim implementacijama
+ */
+public interface Specifikacija {
+    Scanner sc = new Scanner(System.in);
+
+
+    /**
+     * metoda stampaj stampa sadrzaj rasporeda
+     * @params nista
+     */
     void stampaj();
+
+    /**
+     * exportujRaspored ekspotuje raspored u tipa koji korisnik odluci preko stringa tip
+     * @param filename moze biti koje god
+     * @param type moze biti json,pdf,csv
+     */
     void exportujRaspored(String filename,String type);
+
+    /**
+     * radi pretragu po rasporedu sa vratiFiltrirano metodom kojom provjerava sample koji je vracen
+     * @param uslovPretrage string po kome se poziva metoda vratiFiltrirano
+     */
     void pretraziRaspred(String uslovPretrage);
+    /**
+     * sacuvajRaspored vrsi metodu refresh nad bilo koji novonastalim promijenama nad rasporedom
+     * fakticki restrukturira ga sve sa listama i hashMapama
+     */
     void sacuvajRaspored();
+
+    /**
+     * Ucitava fajl i pravi Raspored objekat i u njemu stavlja sve podatke
+     * @param filename ime fajla koji otvaramo i citamo
+     * @return vraca instancu Rasporeda koja je napravljena na osnovu ucitanog fajla
+     */
     Raspored ucitajRaspored(String filename);
 
     //da bi refreshovao potrebne su mi liste dogadjaja
